9130eed9955bceb3bd8503190f27ae3c9c1a7476
Christoph Osswald <christoph.osswald@dvzo.ch>
2022-03-15 15:10:09 +0100

    Change router directory

--- dist/_nuxt/static/1647353180/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkiplus\u002Fdist\u002F_nuxt\u002Fstatic\u002F1647353180",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fkiplus\u002Fdist\u002F",assetsPath:"\u002Fkiplus\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
